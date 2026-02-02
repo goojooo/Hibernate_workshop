edc7573078a83f04ed4440f7e8b3417351ee50bb
Nayan.Dahare <nayandahare02@gmail.com>
2026-02-02 17:15:05 +0530

    first commit

---
+++ proj1_hiber_Maven/src/main/java/org/itp/proj1_hiber_Maven/App.java
@@ -0,0 +1,13 @@
+package org.itp.proj1_hiber_Maven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
