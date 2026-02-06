952475e5af74345bd101a62fcc7cd02bf2957dcb
Nayan.Dahare <nayandahare02@gmail.com>
2026-02-06 19:48:35 +0530

    CJ

--- CORE_JAVA/src/patterns/Pyramid.java
+++ CORE_JAVA/src/patterns/Pyramid.java
@@ -16,19 +16,39 @@ public class Pyramid {
 //				}
 //			}System.out.println();
 //		}
-		int n = 11;
-
-		for (int i = 1; i <= n; i++) {
-		    for (int j = 1; j <= n - i; j++) {
-		        System.out.print(" ");
-		    }
-		    for (int k = 1; k <= 2 * i - 1; k++) {
-		        System.out.print("*");
-		    }
-		    System.out.println();
+//		int n = 11;
+//
+//		for (int i = 1; i <= n; i++) {
+//		    for (int j = 1; j <= n - i; j++) {
+//		        System.out.print(" ");
+//		    }
+//		    for (int k = 1; k <= 2 * i - 1; k++) {
+//		        System.out.print("*");
+//		    }
+//		    System.out.println();
+//		}
+//
+//	}
+//		---------------------------------------------------------------------
+		int n = 5 ;
+		for(int i = 1 ; i <= n ; i++)
+		{
+			for(int j = 1 ; j <= n * 2; j++)
+			{
+				if(j >= n - i + 1 && j <= n + i - 1)
+				{					
+					System.out.print("*");
+				}
+				else {
+					
+					System.out.print(" ");
+				}
+			}
+			System.out.println();
 		}
-
 	}
+		
+		
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
