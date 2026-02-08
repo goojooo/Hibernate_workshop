3a7d047fc66177aa8d3a714f5e4197081d2931e2
Nayan.Dahare <nayandahare02@gmail.com>
2026-02-08 14:53:44 +0530

    oops Constructor

--- CORE_JAVA/src/opps/constructor/BankAccount.java
+++ CORE_JAVA/src/opps/constructor/BankAccount.java
@@ -1,9 +1,11 @@
 package opps.constructor;
 
+import java.util.Scanner;
+
 public class BankAccount {
 	int accountNumber;
 	String accountHolderName;
-	double balance = 1000.0;
+	double balance ;
 	
 	BankAccount(int accountNumber, String accountHolderName, double balance)
 	{
@@ -11,10 +13,58 @@ public class BankAccount {
 		this.accountHolderName = accountHolderName;
 		this.balance = balance;
 	}
+	
+	void deposite(double deposite)
+	{
+		 
+		balance += deposite ;
+		System.out.println("Updated balance successfully: ");
+	}
+	
+	void withdraw(double withdraw)
+	{
+		if(balance >= withdraw)
+		{
+			balance = balance - withdraw;
+			System.out.println("Withdraw balance successfully: ");
+		}else
+		{
+		    System.out.println("Insufficient Balance!");
+		}
+
+	}
+	
+	void displayBalance()
+	{
+		System.out.println("Current balance : " + balance);
+	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter account number :");
+		int accNo = sc.nextInt();
+		sc.nextLine(); // clean buffer
+		
+		System.out.println("Enter Account holder name : ");
+		String name = sc.nextLine();
+		
+		
+		System.out.println("Enter deposite number : ");
+		double bal = sc.nextDouble();
+		BankAccount ba = new BankAccount(accNo, name,bal);
+		double dep = sc.nextDouble();
+		
+		ba.deposite(dep);
+		ba.displayBalance();
+		
+		System.out.println("Enter withdraw amount : ");
+		double with = sc.nextDouble();
+		ba.withdraw(with);
+		ba.displayBalance();
+		
+		sc.close();
+		
 	}
 
 }
