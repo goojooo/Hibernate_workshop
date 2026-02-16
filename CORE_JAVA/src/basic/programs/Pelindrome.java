e3eb06ac3338c0ace5d2dc825ad2376f7b268cba
Nayan.Dahare <nayandahare02@gmail.com>
2026-02-17 00:04:25 +0530

    pelindrom num practice

--- CORE_JAVA/src/basic/programs/Pelindrome.java
+++ CORE_JAVA/src/basic/programs/Pelindrome.java
@@ -3,9 +3,10 @@ package basic.programs;
 public class Pelindrome {
 
 	public static void main(String[] args) {
-		int num = 123456789;
-		int rev = 0, ori;
-		ori = rev;
+		int num, ori , rev = 0;
+		num = 121;
+		ori = num;
+		
 		System.out.println("The original number is : " + num);
 		for(; num != 0 ; num /= 10)
 		{
@@ -14,9 +15,9 @@ public class Pelindrome {
 		}
 		if(ori == rev){
 			
-			System.out.println("It is pelindrome : " + rev);
+			System.out.println("It is pelindrome : " + ori);
 		}else {
-			System.out.println("It is not pelindrome : " + rev);
+			System.out.println("It is not pelindrome : " + ori);
 		}
 
 	}
